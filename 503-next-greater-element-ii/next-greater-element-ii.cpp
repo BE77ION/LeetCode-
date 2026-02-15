162ca57c4334e9990b39d3b1ec855092f992d362
PAWANDEEP SINGH <157210307+BE77ION@users.noreply.github.com>
2026-02-15 23:19:49 +0530

    Time: 147 ms (5.06%) | Memory: 27.9 MB (94.71%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int> ans(nums.size(),-1);
-        
-        for(int i=0;i<nums.size();i++){
-            int currele=nums[i];
-            for(int j=1;j<nums.size();j++){
-                int ind=(i+j)%nums.size();
-                if(nums[ind]>currele){
-                    ans[i]=nums[ind];
+        int n=nums.size();
+        vector<int>ans(n,-1);
+        for(int j=0;j<nums.size();j++){
+            for(int i=1;i<nums.size();i++){
+                int ind=(j+i)%n;
+                if(nums[ind]>nums[j]){
+                    ans[j]=nums[ind];
                     break;
                 }
+                
             }
         }
         return ans;
