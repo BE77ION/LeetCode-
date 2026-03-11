d9c5c3056381084e7c602074b7f57b602f8f26b4
PAWANDEEP SINGH <157210307+BE77ION@users.noreply.github.com>
2026-03-11 22:39:07 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (54.32%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
-    int frst(vector<int>& nums, int target){
-        int left=0;
-        int right=nums.size()-1;
-        int ans=-1;
-        while(left<=right){
-            int mid=(left+right)/2;
-            if(nums[mid]>=target){
-                ans=mid;
-                right=mid-1;
-            }else{
-                left=mid+1;
+    int binarysearch(vector<int>& nums, int target,bool searchleft) {
+        int left = 0;
+        int n = nums.size();
+        int right = n - 1;
+        int ex = -1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                ex = mid;
+                if (searchleft) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
             }
         }
-        return ans;
-    }
-    int lst(vector<int>& nums, int target){
-        int left=0;
-        int right=nums.size()-1;
-        int ans=-1;
-        while(left<=right){
-            int mid=(left+right)/2;
-            if(nums[mid]<=target){
-                ans=mid;
-                left=mid+1;
-            }else{
-                right=mid-1;
-            }
-        }
-        return ans;
+        return ex;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-       int first=frst(nums,target);
-       if(first==-1||nums[first]!=target)return {-1,-1};
-       int last=lst(nums,target);
         
-       return {first,last};
+        int first=binarysearch(nums,target,true);
+        int last=binarysearch(nums,target,false);
+
+        return {first,last};
     }
 };
