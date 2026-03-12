b31321964b03abab5f23233de0968a3e0b857f27
PAWANDEEP SINGH <157210307+BE77ION@users.noreply.github.com>
2026-03-12 23:15:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (35.23%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -4,12 +4,20 @@ public:
         int left=0;
         int n=nums.size();
         int right=n-1;
+        if(n==1)return 0;
+        if(nums[0]>nums[1]){
+            return 0;
+        }
+        if(nums[n-2]<nums[n-1])return n-1;
         while(left<right){
             int mid=left+(right-left)/2;
-            if(nums[mid]>nums[mid+1]){
-                right=mid;
-            }else{
+            if(nums[mid]>nums[mid+1]&&nums[mid]>nums[mid-1]){
+                return mid;
+            }
+            else if(nums[mid]>nums[mid-1]){
                 left=mid+1;
+            }else{
+                right=mid;
             }
         }
         return left;
