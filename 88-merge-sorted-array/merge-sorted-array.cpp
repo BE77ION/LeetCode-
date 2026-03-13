a03f0e68a09aa74f864d7ec8f28160f9b8b1f01d
PAWANDEEP SINGH <157210307+BE77ION@users.noreply.github.com>
2026-03-13 23:59:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (40.97%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int>temp;
-        int i=0;
-        int j=0;
-        while(i<m&&j<n){
-            if(nums1[i]<=nums2[j]){
-                temp.push_back(nums1[i++]);
+        int left=m-1;
+        int right=0;
+        while(left>=0&&right<n){
+            if(nums1[left]>nums2[right]){
+                swap(nums1[left],nums2[right]);
+                right++;
+                left--;
             }else{
-                temp.push_back(nums2[j++]);
+                break;
             }
         }
-        while(i<m){
-            temp.push_back(nums1[i++]);
-        }
-        while(j<n){
-            temp.push_back(nums2[j++]);
-        }
-
-        for(int i=0;i<m+n;i++){
-            nums1[i]=temp[i];
+        sort(nums1.begin(),nums1.begin()+m);
+        sort(nums2.begin(),nums2.end());
+        for(int i=m;i<m+n;i++){
+            nums1[i]=nums2[i-m];
         }
     }
 };
-//tc: o(n+m)+o(n+m);
-//sc:o(n+m);
+// approch 2 better
+//here tc is O(min(n,m))+o(nlogn)+o(nlogn);
+//sc=o(1);
