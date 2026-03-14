70e8349f59f6664fb318495406a694a6bc3aeb96
PAWANDEEP SINGH <157210307+BE77ION@users.noreply.github.com>
2026-03-14 21:48:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (60.42%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+int possible(vector<int>& nums, int k,int m){
+    int sum=0;
+    int split=1;
+    for(int i=0;i<nums.size();i++){
+        if(sum+nums[i]<=m){
+            sum+=nums[i];
+        }else{
+            sum=nums[i];
+            split++;
+        }
+    }
+    return split;
+}
+    int splitArray(vector<int>& nums, int k) {
+        int maxe=*max_element(nums.begin(),nums.end());
+        int maxc=accumulate(nums.begin(),nums.end(),0);
+        int left=maxe;
+        int right=maxc;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(possible(nums,k,mid)<=k){
+                right=mid-1;
+            }else{
+               left=mid+1;
+            }
+        }
+        return left;
+    }
+};
