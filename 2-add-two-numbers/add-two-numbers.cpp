ab4f82ec47e0dabd88b9c4347b2918b5b7efd716
PAWANDEEP SINGH <157210307+BE77ION@users.noreply.github.com>
2026-03-18 22:55:27 +0530

    Time: 3 ms (34.94%) | Memory: 77.1 MB (45.14%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,37 +11,23 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* temp=l1;
-        vector<int>list1;
-        while(temp!=NULL){
-            list1.push_back(temp->val);
+        ListNode* dummy = new ListNode(0);
+        ListNode* temp = dummy;
+        int carry = 0;
+        while (l1 || l2 || carry) {
+            int sum = carry;
+            if (l1 != NULL) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != NULL) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            temp->next = new ListNode(sum % 10);
+            carry = sum / 10;
             temp=temp->next;
         }
-        temp=l2;
-        vector<int>list2;
-        while(temp!=NULL){
-            list2.push_back(temp->val);
-            temp=temp->next;
-        }
-        vector<int>ans;
-        int left=0;
-        int right=0;
-        int carry=0;
-        int i=0;
-        while(left<list1.size()||right<list2.size()||carry){    
-            int sum=carry;
-            if(left<list1.size())sum+=list1[left++];
-            if(right<list2.size())sum+=list2[right++];
-                ans.push_back(sum%10);
-                carry=sum/10;
-        }
-        ListNode* newnode=new ListNode(0);
-        ListNode* curr=newnode;
-
-        for(auto i:ans){
-            curr->next=new ListNode(i);
-            curr=curr->next;
-        }
-        return newnode->next;
+        return dummy->next;
     }
 };
