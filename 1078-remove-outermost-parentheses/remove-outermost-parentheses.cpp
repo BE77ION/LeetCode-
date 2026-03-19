f4bcac2926552b64f1f9b5b705e2d9e16d43adb9
PAWANDEEP SINGH <157210307+BE77ION@users.noreply.github.com>
2026-03-19 18:24:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (80.92%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        int start=0;
+        
         int count=0;
         string res;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='(')count++;
-            else count--;
-            if(count==0){
-                res+=s.substr(start+1,i-start-1);
-                start=i+1;
+            if(s[i]=='('){
+                if(count>0){
+                    res+=s[i];
+                }
+                count++;
+            }else{
+                count--;
+                if(count>0){
+                    res+=s[i];
+                }
             }
         }
         return res;
