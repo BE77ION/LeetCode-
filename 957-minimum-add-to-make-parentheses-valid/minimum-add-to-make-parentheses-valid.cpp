17c67932bdd5fcdca018f006cfb1d3cf75e3365a
PAWANDEEP SINGH <157210307+BE77ION@users.noreply.github.com>
2026-03-23 18:51:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (33.64%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>st;
+        for(auto i:s){
+    
+            if(!st.empty()&&st.top()=='('&&i==')'){
+                st.pop();
+                continue;
+            }
+            st.push(i);
+
+        }
+        return st.size();
+    }
+};
