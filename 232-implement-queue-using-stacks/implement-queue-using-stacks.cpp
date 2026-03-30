ab10a6ed0811f3f26feb1b49b85489a821cae3e2
PAWANDEEP SINGH <157210307+BE77ION@users.noreply.github.com>
2026-03-30 21:25:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (96.28%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,36 +1,40 @@
 class MyQueue {
 public:
-stack<int>s1;
-stack<int>s2;
+stack<int>s1; stack<int>s2;
     MyQueue() {
         
     }
     
     void push(int x) {
-        while(s1.size()){
-            s2.push(s1.top());
-            s1.pop();
-        }
         s1.push(x);
-        while(s2.size()){
-            s1.push(s2.top());
-            s2.pop();
-        }
     }
     
     int pop() {
-        int val = s1.top();
-    s1.pop();
-    return val;
-
+        if(s2.empty()){
+           while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+           }
+        }
+        int val=s2.top();
+        s2.pop();
+        return val;
     }
     
     int peek() {
-       return s1.top();
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        return s2.top();
+        
     }
     
     bool empty() {
-       return s1.empty();
+       return  s1.empty()&&s2.empty();
     }
 };
 
