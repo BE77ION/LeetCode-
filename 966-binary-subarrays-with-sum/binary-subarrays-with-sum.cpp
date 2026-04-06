17e002da2a583db0623d6d90cb7fd9cf9085161e
PAWANDEEP SINGH <157210307+BE77ION@users.noreply.github.com>
2026-04-06 21:13:19 +0530

    Time: 10 ms (38.88%) | Memory: 38.6 MB (27.02%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int k) {
-        return atmost(nums,k)-atmost(nums,k-1);
-
-    }
-    int atmost(vector<int>& nums, int k){
-        int n=nums.size();
+        unordered_map<int,int>mp;
+        mp[0]=1;
+        int sum=0;
         int count=0;
-        if(k < 0) return 0;
-        int l=0;int r=0;int sum=0;
-        while(r<n){
-            sum+=nums[r];
-            while(sum>k){
-                sum=sum-nums[l];
-                l++;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(mp.count(sum-k)){
+                count+=mp[sum-k];
             }
-            if(sum<=k){
-                count=count+(r-l+1);
-            }
-            r++;
+            mp[sum]++;
         }
         return count;
     }
