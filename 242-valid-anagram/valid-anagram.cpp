1a1b1c422393d32628b6628743e97066b52159ba
PAWANDEEP SINGH <157210307+BE77ION@users.noreply.github.com>
2026-05-21 11:18:22 +0530

    Time: 7 ms (18.84%) | Memory: 9.5 MB (91.83%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         sort(s.begin(),s.end());
-         sort(t.begin(),t.end());
-
-         if(s==t)return true;
-         return false;
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
